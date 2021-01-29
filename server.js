277e0f12109d4581eab501e41872d4e7b7a804a8
Zarrouk Malek <73857284+malek8@users.noreply.github.com>
2021-01-29 12:31:50 +0100

    footbal

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/eshop'));
+app.use(express.static('./dist/footbal'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/eshop/'}),
+    res.sendFile('index.html', {root: 'dist/footbal/'}),
 );
 
 app.listen(process.env.PORT || 8080);
